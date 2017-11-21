5b4adf39df0696605061f16823ad81fbff0c81db
Ibrahim Olanigan <salalite@gmail.com>
2017-11-21 15:11:49 -0500

    Created Contact model

---
+++ server/models/contact.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var Contact = sequelize.define("contacts", {
+    id:   { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true
+     },
+    first_name: {
+         type: DataTypes.STRING,
+         allowNull: false
+    },
+    last_name: {
+         type: DataTypes.STRING,
+         allowNull: false
+    },
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING,
+  },
+  {timestamps: false}
+);
+  
+  return Contact;
+};
